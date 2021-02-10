879178c22b848b10c9c9b35b2dfa4fa698bac9db
kateabr <kateabr@yandex.ru>
2021-02-10 19:36:31 +0300

    fixing docker file

--- Dockerfile
+++ Dockerfile
@@ -8,9 +8,4 @@ COPY src src
 COPY dictionaries/yarxi.jtdb dictionaries/yarxi.jtdb
 COPY dictionaries/warodai.jtdb dictionaries/warodai.jtdb
 
-ENV PORT=5000
-ENV FLASK_RUN_PORT=$PORT
-
-EXPOSE $PORT
-
 ENTRYPOINT PYTHONPATH=$PYTHONPATH:/app python src/flask_demo.py
--- src/flask_demo.py
+++ src/flask_demo.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 from flask import Flask, render_template, request
@@ -27,4 +28,5 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
